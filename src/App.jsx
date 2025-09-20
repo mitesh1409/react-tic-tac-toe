697db80433d2649e28faf7489dbeb685c5446ce7
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-09-20 15:56:37 +0530

    Update App component - add Board component and reuse Square component in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+  return (
+    <button className="square">X</button>
+  );
 }
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
+
+export default Board;

e34b2b675b327ee41288d18881d0f745c066d50d
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-09-26 18:38:31 +0530

    Refactor component to lift the state up, move it to parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function markSquare() {
-    setValue('X');
-  }
-
+function Square({value, onSquareClick}) {
   return (
     <button
       className="square"
-      onClick={markSquare}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -18,24 +12,32 @@ function Square() {
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleSquareClick() {
+    const updatedSquares = [...squares];
+    updatedSquares[0] = 'X';
+    setSquares(updatedSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleSquareClick} />
+        <Square value={squares[1]} onSquareClick={handleSquareClick} />
+        <Square value={squares[2]} onSquareClick={handleSquareClick} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleSquareClick} />
+        <Square value={squares[4]} onSquareClick={handleSquareClick} />
+        <Square value={squares[5]} onSquareClick={handleSquareClick} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleSquareClick} />
+        <Square value={squares[7]} onSquareClick={handleSquareClick} />
+        <Square value={squares[8]} onSquareClick={handleSquareClick} />
       </div>
     </>
   );

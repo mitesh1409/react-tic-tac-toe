804a35de026d2aaeb9572f0f5ca57d6a3c1a0150
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-06-24 19:21:29 +0530

    Calculate winner and show status accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleSquareClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
 
@@ -17,8 +17,13 @@ export default function Board() {
     setPlayer(player === 'X' ? 'O' : 'X');
   }
 
+  const winner = calculateWinner(squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${player}`;
+
   return (
     <>
+      <div className="status">{status}</div>
+
       <div className="board-row">
         <Square value={squares[0]} onClick={() => handleSquareClick(0)} />
         <Square value={squares[1]} onClick={() => handleSquareClick(1)} />
@@ -36,4 +41,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }

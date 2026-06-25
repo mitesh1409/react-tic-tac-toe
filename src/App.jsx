b5efd7f768b6a3ae658b45102ea599b0217a7acb
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2026-06-25 14:55:38 +0530

    Implement game history

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,48 @@ import Square from './Square';
 
 export default function Game() {
   const [player, setPlayer] = useState('X');
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [history, setHistory] = useState([]);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [historyMode, setHistoryMode] = useState(false);
 
   function handlePlay(newSquares) {
+    setSquares(newSquares);
     setHistory([...history, newSquares]);
     setPlayer(player === 'X' ? 'O' : 'X');
   }
 
+  function jumpTo(moveId) {
+    setSquares(history[moveId]);
+    setHistoryMode(moveId === (history.length - 1) ? false : true);
+  }
+
+  const moves = history.map((squares, index) => {
+    const description = `Go to move # ${index + 1}`;
+
+    return (
+      <li key={index}>
+        <button onClick={() => jumpTo(index)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board player={player} squares={currentSquares} onPlay={handlePlay} />
+        <Board player={player} squares={squares} onPlay={handlePlay} historyMode={historyMode} />
       </div>
       <div className="game-info">
         <ol>
-          <li>One</li>
-          <li>Two</li>
-          <li>Three</li>
+          {moves}
         </ol>
       </div>
     </div>
   );
 }
 
-function Board({ player, squares, onPlay }) {
+function Board({ player, squares, onPlay, historyMode }) {
   function handleSquareClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
+    if (squares[i] || calculateWinner(squares) || historyMode) {
       return;
     }
 
